f224d9674d988c8ba8ce11d1df17a1056a79293c
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-02 16:03:00 -0700

    Update cpu.go

--- thread/cpu.go
+++ thread/cpu.go
@@ -8,3 +8,9 @@ func init() {
 	CPUThread = flag.Int("t", -1, "prefork child id")
 	flag.Parse()
 }
+
+func SetCPUAffinity(cpu int) error {
+	var newMask unix.CPUSet
+	newMask.Set(cpu)
+	return unix.SchedSetaffinity(0, &newMask)
+}
