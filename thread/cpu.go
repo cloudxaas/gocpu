5c3fc09cb104119993b56fdeca3fdc9468a7028a
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-03 10:56:54 -0700

    Update cpu.go

--- thread/cpu.go
+++ thread/cpu.go
@@ -9,30 +9,32 @@ import(
 	
 )
 var (
-	//Support up to 32000 cpu cores 
+	//Support up to 65534 cpu cores
 	//because of the way other functions wraps it, 
-        //negative value denotes no change in other func
-	CPUThread *int16
+        //"0" value denotes or master
+	CPUThread *uint16
 )
 
 func init() {
-	CPUThread = flag.Int16P("thread", "t", -1, "prefork child id")
+	CPUThread = flag.Uint16P("thread", "t", 0, "prefork child id")
 	flag.Parse()
 }
 
-func SetCPUAffinity(cpu int16) error {
+//cpu core 0 = 1
+//cpu core 1 = 2
+func SetCPUAffinity(cpu uint16) error {
 	var newMask unix.CPUSet
-	newMask.Set(int(cpu))
+	newMask.Set(int(cpu)-1)
 	return unix.SchedSetaffinity(0, &newMask)
 }
 
 //hash based on fnv1a32, getting hashed cpu id
-func CPUHash(k []byte) int16 {
-	return int16(fnv1a32.Hash(k) % uint32(runtime.NumCPU()))
+func CPUHash(k []byte) uint16 {
+	return uint16(fnv1a32.Hash(k) % uint32(runtime.NumCPU()))
 }
 
-func IsCurrentCPUID(id int16) uint8 {
-	if id == int16(*CPUThread) {
+func IsCurrentCPUID(id uint16) uint8 {
+	if id == *CPUThread {
 		return 1
 	}
 	return 0
