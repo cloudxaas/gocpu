bc3fdbb9efc30783a2dd771d84e7c8a69f137a90
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-03 03:57:00 -0700

    Update cpu.go

--- thread/cpu.go
+++ thread/cpu.go
@@ -5,17 +5,18 @@ import(
 	"golang.org/x/sys/unix"
 )
 var (
-	//though 65535, but actually support up to 32000 cpu cores 
-	//because of the way other functions wraps it
-	CPUThread *uint16
+	//Support up to 32000 cpu cores 
+	//because of the way other functions wraps it, 
+        //negative value denotes no change in other func
+	CPUThread *int16
 )
 
 func init() {
-	CPUThread = flag.Uint16("t", -1, "prefork child id"))
+	CPUThread = flag.Int16("t", -1, "prefork child id"))
 	flag.Parse()
 }
 
-func SetCPUAffinity(cpu uint16) error {
+func SetCPUAffinity(cpu Int16) error {
 	var newMask unix.CPUSet
 	newMask.Set(int(cpu))
 	return unix.SchedSetaffinity(0, &newMask)
