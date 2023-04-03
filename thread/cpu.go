6cec5835988c7fc0a6ac3f1a88c55d943bbc1077
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-03 05:40:17 -0700

    Update cpu.go

--- thread/cpu.go
+++ thread/cpu.go
@@ -2,6 +2,7 @@ package cxcputhread
 
 import(
 	"golang.org/x/sys/unix"
+	"github.com/cloudxaas/gohash/fnv1a32"
 	flag "github.com/spf13/pflag"
 )
 var (
@@ -20,4 +21,9 @@ func SetCPUAffinity(cpu int16) error {
 	var newMask unix.CPUSet
 	newMask.Set(int(cpu))
 	return unix.SchedSetaffinity(0, &newMask)
+}
+
+//hash based on fnv1a32, getting hashed cpu id
+func CPUHash(k []byte) int16 {
+	return int16(fnv1a32.Hash(k) % uint32(runtime.NumCPU()))
 }
