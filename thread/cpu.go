13d7cf727c636c772ff31140b41b55da4a270b74
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2024-03-15 07:01:31 -0400

    Update cpu.go

--- thread/cpu.go
+++ thread/cpu.go
@@ -1,36 +1,37 @@
 package cxcputhread
 
-import(
+import (
 	"runtime"
-	
+	"github.com/zeebo/xxh3" // Updated import for xxh3
 	"golang.org/x/sys/unix"
-	"github.com/cloudxaas/gohash/fnv1a32"
-	flag "github.com/spf13/pflag"
-	
-)
-var (
-	//Support up to 65534 cpu cores
-	//because of the way other functions wraps it, 
-        //"0" value denotes or master
-	CPUThread uint16
+	"github.com/spf13/pflag"
 )
 
-func init() {
-	CPUThread = *flag.Uint16P("thread", "t", 0, "prefork child id")
-	flag.Parse()
+var (
+	NumCPU uint16
+	CPUThread uint16 // Support up to 65534 CPU cores, "0" denotes master or default
+)
+
+// This function should be called from the main package or higher-level logic to define flags.
+func DefineFlags() {
+	pflag.Uint16VarP(&CPUThread, "CPUThread", "t", 0, "prefork child id")
 }
 
-//cpu core 0 = 1
-//cpu core 1 = 2
+func init() {
+	NumCPU = runtime.NumCPU()
+}
+
+//cpu core 0 == 1
+//cpu core 1 == 2
 func SetCPUAffinity(cpu uint16) error {
 	var newMask unix.CPUSet
-	newMask.Set(int(cpu)-1)
+	newMask.Set(int(cpu) - 1)
 	return unix.SchedSetaffinity(0, &newMask)
 }
 
-//hash based on fnv1a32, getting hashed cpu id
+// Updated function to use xxh3 for hashing with AVX2 optimization when available.
 func CPUHash(k []byte) uint16 {
-	return uint16(fnv1a32.Hash(k) % uint32(runtime.NumCPU()))
+	return uint16(xxh3.Hash(k) % uint64(runtime.NumCPU()))
 }
 
 func IsCurrentCPUID(id uint16) uint8 {
